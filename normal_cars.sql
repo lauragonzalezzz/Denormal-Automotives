d06c07366a269b375f8fb3bae4c45b90b9f87cc3
Laura <lg3295@gmail.com>
2016-04-18 17:06:02 -1000

    sets up years, models, makes, and vehicles tables

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,34 +1,34 @@
 -- CREATE ROLE normal_user;
 -- CREATE DATABASE normal_cars;
 -- \c normal_cars;
+-- \i /home/laura/DevLeague/Denormal-Automotives/scripts/denormal_data.sql;
 
--- CREATE TABLE IF NOT EXISTS vehicle_year
--- (
---   id SERIAL,
---   year INTEGER NOT NULL,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE years (
+  id SERIAL PRIMARY KEY,
+  year VARCHAR(255) NOT NULL,
+);
 
-CREATE TABLE IF NOT EXISTS vehicle_makes
-(
-  id SERIAL,
-  PRIMARY KEY (id),
-  make_code VARCHAR(255),
-  make_title VARCHAR(255)
+CREATE TABLE models (
+  id SERIAL PRIMARY KEY,
+  model_code VARCHAR(255) NOT NULL,
+  model_title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE makes (
+  id SERIAL PRIMARY KEY,
+  makes_code VARCHAR(255) NOT NULL,
+  makes_title VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS vehicle_models
-(
-  id SERIAL,
-  PRIMARY KEY (id),
-  model_code VARCHAR(255),
-  model_title VARCHAR(255)
+CREATE TABLE vehicles (
+  id SERIAL PRIMARY KEY,
+  year REFERENCES years(year),
+  model_code REFERENCES models(model_code),
+  model_title REFERENCES models(model_title),
+  make_code REFERENCES makes(makes_code),
+  make_title REFERENCES makes(makes_title)
 );
 
--- CREATE TABLE IF NOT EXISTS vehicles
--- (
---   id SERIAL,
---   PRIMARY KEY (id),
---   year,
--- );
+
+
