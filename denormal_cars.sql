a5da7bca58330191aa69386caf83d2ca16170002
Laura <lg3295@gmail.com>
2016-04-19 15:16:22 -1000

    modifies select query

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -3,11 +3,11 @@
 -- \c denormal_cars;
 -- \i /home/laura/DevLeague/Denormal-Automotives/scripts/denormal_data.sql;
 
-\dS car_models;
+-- \dS car_models;
 
 -- SELECT make_title, year FROM car_models WHERE year= 2001;
 -- SELECT make_title, model_title FROM car_models WHERE year= 2000;
--- SELECT * FROM car_models WHERE make_title= 'Yugo';
+-- SELECT * FROM car_models WHERE make_title= 'Yugo' AND year BETWEEN 2000 AND 2001;
 
 -- SELECT DISTINCT make_title FROM car_models;
 
@@ -16,3 +16,6 @@
 -- SELECT DISTINCT make_code, model_code, model_title, year FROM car_models WHERE make_code= 'LAM';
 
 -- SELECT DISTINCT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
+
+--make_code and make_title are the same
+--model_code and model_title are the same
